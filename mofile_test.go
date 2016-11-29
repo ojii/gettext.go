80b696009bfb5935e3c9771864e9242a487e9f30
Michael Vogt <mvo@ubuntu.com>
2016-11-29 11:00:43 +0100

    Deal with missing plural forms string in mo files
    If a mo file does not contain plural forms for the language the
    code in NGettext() should use the default germanic plural rule
    just like the C gettext implemenation in glibc
    (c.f. intl/dcigettext.c:g16 in glibc 2.24).

--- mofile_test.go
+++ mofile_test.go
@@ -41,6 +41,25 @@ func TestEnNGettext(t *testing.T) {
 	)
 }
 
+func TestNGettextBrokenMoNoCrash(t *testing.T) {
+	file, err := os.Open("testdata/en-no-plural-forms/messages.mo")
+	if err != nil {
+		t.Fatal(err)
+	}
+	catalog, err := ParseMO(file)
+	if err != nil {
+		t.Fatal(err)
+	}
+	assert_equal(t,
+		catalog.NGettext("order %d beer", "order %d beers", 1),
+		"%d beer please",
+	)
+	assert_equal(t,
+		catalog.NGettext("order %d beer", "order %d beers", 2),
+		"%d beers please",
+	)
+}
+
 func TestJaGettext(t *testing.T) {
 	file, err := os.Open("testdata/ja/messages.mo")
 	if err != nil {
